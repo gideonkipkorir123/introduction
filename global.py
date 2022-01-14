5769048be01b258b4d98d0b8a3ff001ba1874c35
unknown <HP@PC.HP>
2022-01-14 18:47:17 +0300

    scope and global

---
+++ global.py
@@ -0,0 +1,25 @@
+# def my_function():
+#     global var
+#     var = 2
+#     print("Do I know that variable?", var)
+
+
+# var = 1
+# my_function()
+# print(var)
+# def my_assignment():
+#     global var
+#     var=5
+#     print('do you recognize',var)
+# var=23
+# my_assignment()
+# print(var) 
+def math(x):
+
+    var=x
+    return x*3
+
+math(9)
+
+print('the returned value',math(9))
+
