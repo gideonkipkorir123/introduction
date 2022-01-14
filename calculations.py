91adf13a118211029636049604cdd1efa4fe7161
unknown <HP@PC.HP>
2022-01-14 18:51:30 +0300

    calculations

--- calculations.py
+++ calculations.py
@@ -1,5 +1,8 @@
-def bmi(weight, height):
-    return weight / height ** 2
+# def bmi(weight, height):
+#     return weight / height ** 2
 
 
-print(bmi(52.5, 1.65))
+# print(bmi(52.5, 1.65))
+def calculations(metres,centimetres):
+    return metres/centimetres
+print (calculations(1,1000))
