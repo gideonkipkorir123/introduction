299539512a01bc3611a4ce84aa7285ce49c0abb1
evankarani <evanskarani069@gmail.com>
2022-01-27 16:57:08 +0300

    reading

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,10 @@
-cars={'model':'mustad','brand':'ford','year':1989}
-print(cars['year'])
-print(type(cars))
+# cars={'model':'mustad','brand':'ford','year':1989}
+# print(cars['year'])
+# print(type(cars))
+# each key must be unique - it's not possible to have more than one key of the same value;
+# a key may be any immutable type of object: it can be a number (integer or float), or even a string, but not a list;
+# a dictionary is not a list - a list contains a set of numbered values, while a dictionary holds pairs of values;
+# the len() function works for dictionaries, too - it returns the numbers of key-value elements in the dictionary;
+# a dictionary is a one-way tool - if you have an English-French dictionary, you can look for French equivalents of English terms, but not vice versa.
+student={'department':'electrical','class:':'electrical engineering','yr':5}
+print(student)
--- tuple.py
+++ tuple.py
@@ -1,6 +1,22 @@
 # this is an immutable data 
-tuple=(1,2,3,4,)
-tuple_2=(5,6,7,8,9,41)
-# print(tuple[-1])
-for elem in tuple:
-    print(elem)
+# tuple=(1,2,3,4,)
+# tuple_2=(5,6,7,8,9,41)
+# # print(tuple[-1])
+# for elem in tuple:
+#     print(elem)
+
+
+# the len() function accepts tuples, and returns the number of elements contained inside;
+# the + operator can join tuples together (we've shown you this already)
+# the * operator can multiply tuples, just like lists;
+# the in and not in operators work in the same way as in lists.
+my_tuple = (1, 10, 100)
+
+t1 = my_tuple + (1000, 10000)
+t2 = my_tuple * 3
+
+print(len(t2))
+print(t1)
+print(t2)
+print(10 in my_tuple)
+print(-10 not in my_tuple)
