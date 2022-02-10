5bd505917fc8709bc38ca68da7fffa29002fb3aa
evankarani <evanskarani069@gmail.com>
2022-02-10 14:21:30 +0300

    import function

---
+++ cases.py
@@ -0,0 +1,7 @@
+name='gideon'
+print(name.upper())
+print(name.upper().islower())
+print(name.index('g'))
+print(name.replace ('gideon','mike'))
+age=132
+print(str(age )  ,'is the year that i was born')
---
+++ import.py
@@ -0,0 +1,7 @@
+from math import *
+from sre_constants import MAX_REPEAT
+print(floor(4.55555))
+print(round(4.55555))
+print(max(4.55555,4.555556))
+print(min(4.55555,8888888888))
+print(floor(4.5))
