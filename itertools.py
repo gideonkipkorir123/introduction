7ed7c38a7a764d667bab6c49a62dcb57a8d533c0
evankarani <evanskarani069@gmail.com>
2022-03-01 14:40:57 +0300

    itertolls

---
+++ itertools.py
@@ -0,0 +1,35 @@
+# itertools are products and permutations that accumulate infinite itrators
+# they consis product,permuation,combination,accumulate,group by and iterators
+# product gives the possible combination of characters in a list
+# from itertools import product
+# from timeit import repeat
+# a=[1,2]
+# b=[3,4]
+# x=product(a,b ,repeat=1)
+# print(list(x))
+# from itertools import permutations
+# x=[1,2,3,4]
+# y=permutations(x,4)
+# print(list(y))
+# from itertools import combinations,combinations_with_replacement
+# x=[1,2,3,4,5]
+# y=combinations(x,2)
+# z=combinations_with_replacement(x,2)
+# print(list(z)
+# )
+# print(list(y))
+# from itertools import accumulate
+# import operator
+# x=[1,2,3,4]
+# y=accumulate(x,func=max)
+# print(list(x))
+# print(list(y))
+from itertools import groupby
+students =[{"name":"Tim","age":25,"sex":"male"},
+    {"name":"Gideon","age":25,"sex":"male"},
+
+{"name":"mike","age":25,"sex":"male"
+}]
+y=groupby(students,key=lambda x:x["age"])
+for key,value  in y:
+    print(key,list(value))
